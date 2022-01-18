cd5b0da981fb306287529a0135e47b23ae6c2475
Ajayi John <36191735+john-ladele@users.noreply.github.com>
2022-01-18 16:55:21 +0100

    ADDING A SPECIAL ATTACK
    - Added a special attack function with more intense damage on the monster.
    - Made the special attack function available only after 3 rounds. - Disable button is current round is not divisible by 3

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -7,6 +7,7 @@ Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
+            currentRound: 0,
         };
     },
     computed: {
@@ -20,10 +21,14 @@ Vue.createApp({
                 width: this.playerHealth + '%'
             };
         },
+        mayUseSpecialAttack() {
+            return this.currentRound % 3 !== 0
+        }
     },
     methods: {
         //actions we support in our vue application
         attackMonster() { //math.floor rounds up the result to prevent decimals
+            this.currentRound++;
             const attackValue = getRandomValue(5, 12); //this calculates random numbers between 5(min) and 12(max)
             this.monsterHealth = this.monsterHealth - attackValue;
             this.attackPlayer(); //since it's an auto attack on player when monster is attacked, we then call the attackPlayer method here so it is applied everytime the monster is attacked!
@@ -31,6 +36,12 @@ Vue.createApp({
         attackPlayer() {
             const attackValue = getRandomValue(8, 15); //this calculates random numbers between 8(min) and 15(max) bcos the monster hits harder
             this.playerHealth -= attackValue;
+        },
+        specialAttackMonster() {
+            this.currentRound++;
+            const attackValue = getRandomValue(10, 25); //special damage
+            this.monsterHealth = this.monsterHealth - attackValue;
+            this.attackPlayer();
         },
         playerHealth() {
             const healthAddUp = getRandomValue(2, 7);
